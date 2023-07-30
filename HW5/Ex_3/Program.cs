87a01f99429d5f759f8b86e6b4f1c82fa64e1ef5
Anastasiia <buravlyova14@gmail.com>
2023-07-30 23:53:18 +0300

    Hw5

---
+++ HW5/Ex_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+int arrayLength= 5;
+int [] array= new int [arrayLength];
+
+int max=array[0];
+int min=array[0];
+
+for (int i = 0; i < arrayLength; i++)
+{
+    array[i]= new Random().Next(100);
+    System.Console.Write(array[i]+ " ");
+
+    if(array[i]>max)
+    {
+        array[i]==max;
+    }
+    else if(array[i]<min)
+    {
+        array[i]==min;
+    }
+
+}
+System.Console.WriteLine();
+System.Console.WriteLine(max-min);
+
