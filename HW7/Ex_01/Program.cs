4972296f3ad9de102839d8f3d9338685c1b1ef56
Anastasiia <buravlyova14@gmail.com>
2023-08-08 01:47:27 +0300

    Hw7

---
+++ HW7/Ex_01/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Add a number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Add a number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix= new double [num1, num2];
+
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j]=Convert.ToDouble(new Random().Next(0,10));
+        System.Console.Write(matrix[i,j]+ " ");
+    }
+    System.Console.WriteLine();
+   
+}
